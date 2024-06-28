42f744b719f52e58820b5a3b67658cc0951d0ce7
Sushmithaa-Pandian <sushmithaa2110427@ssn.edu.in>
2024-06-28 18:07:57 +0530

    changes

--- script.js
+++ script.js
@@ -1,81 +1,85 @@
-function getComputerChoice(){
-    let x= Math.random();
-    if(x<0.3){
-        return("ROCK");
-    }
-    else if(0.3<=x && x<0.6){
-        return("PAPER");
-    }
-    else{
-        return("SCISSOR");
-    }
+let humanScore = 0;
+let computerScore = 0;
 
+function getComputerChoice() {
+    let x = Math.random();
+    if (x < 0.3) {
+        return "ROCK";
+    } else if (x < 0.6) {
+        return "PAPER";
+    } else {
+        return "SCISSORS";
+    }
 }
 
+function playRound(humanChoice, computerChoice) {
+    const loseMessages = {
+        "ROCK": "You lose! Rock beats Scissors",
+        "SCISSORS": "You lose! Scissors beat Paper",
+        "PAPER": "You lose! Paper beats Rock"
+    };
+    const winMessage = "You Win!!";
 
-function getHumanChoice(){
-    let y = prompt("Enter your move");
-    let k= y.toUpperCase();
-    return k;
-    
+    if (humanChoice === "ROCK" && computerChoice === "SCISSORS" ||
+        humanChoice === "SCISSORS" && computerChoice === "PAPER" ||
+        humanChoice === "PAPER" && computerChoice === "ROCK") {
+        humanScore++;
+        return winMessage;
+    } else if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else {
+        computerScore++;
+        return loseMessages[humanChoice];
+    }
 }
 
-
-
-
-function playGame(){
-    let humanScore=0;
-    let computerScore=0;
-    function playRound(humanChoice,computerChoice){
-        let j="You lose! Rock beats Scissor";
-        let k="You lose! Scissor beats Paper";
-        let l="You lose! Paper beats Rock";
-        let m="You Win !!";
-        if (humanChoice =="ROCK" && computerChoice =="SCISSOR"){
-            computerScore++;
-            return j;
-            
-        }
-        else if (humanChoice =="SCISSOR" && computerChoice =="PAPER"){
-            computerScore++;
-            return k;
-            
-        }
-        else if (humanChoice =="PAPER" && computerChoice =="ROCK"){
-            computerScore++;
-            return l;
-
-            
-        }
-        else{
-            humanScore++;
-            return m;
-            
-        }
-    }
-
-    for(let x=0;x<5;x++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log("Your move :" , humanSelection);
-        console.log("Computer's move :" , computerSelection);
-        console.log(playRound(humanSelection, computerSelection));
-        
-
-
-    }
-    console.log(humanScore);
-    console.log(computerScore);
-    if (humanScore>computerScore){
-        console.log("You are the winner");
-    }
-    else{
-        console.log("Computer is the winner");
-    }
-    
+function updateScore() {
+    const scoreDiv = document.getElementById("score");
+    scoreDiv.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
 }
 
-console.log(playGame());
+function checkWinner() {
+    const finalResultDiv = document.getElementById("final-result");
+    if (humanScore === 5) {
+        finalResultDiv.textContent = "Congratulations! You won the game!";
+        resetGame();
+    } else if (computerScore === 5) {
+        finalResultDiv.textContent = "Sorry! The computer won the game.";
+        resetGame();
+    }
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    updateScore();
+    document.getElementById("results").innerHTML = "";
+}
+
+function playGame(humanChoice) {
+    const computerChoice = getComputerChoice();
+    const resultMessage = playRound(humanChoice, computerChoice);
+
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = `
+        <p>Your move: ${humanChoice}</p>
+        <p>Computer's move: ${computerChoice}</p>
+        <p>${resultMessage}</p>
+    `;
+
+    updateScore();
+    checkWinner();
+}
+
+// Add event listeners to the buttons
+document.querySelectorAll("#contain button").forEach(button => {
+    button.addEventListener("click", () => {
+        playGame(button.id);
+    });
+});
+
+
+
 
 
 
